f4bb0d115f8f493de5976859969573f0807b83c0
johnathanvidu <johnathan.v@qualisystems.com>
2025-06-08 18:13:20 +0300

    Update variables.tf

--- terraform/create-ec2-instance/variables.tf
+++ terraform/create-ec2-instance/variables.tf
@@ -1,11 +1,5 @@
 variable "instance_type" {
   description = "(Optional) The instance type to use for the instance. defaults to t3.micro"
   type        = string
-  default     = "t3.micro"
-}
-
-variable "ami" {
-  description = "(Optional) AMI to use for the instance. Defaults to latest Ubuntu 18.04 LTS"
-  type        = string
-  default     = ""
+  default     = "t4g.nano"
 }
