92fe14636ea8e080028443d3970d435164df8442
svetaquali <svetlana.c@quali.com>
2025-06-29 10:48:54 +0300

    fix body

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -3,4 +3,4 @@ torque_token=$2
 space=$3
 env_id=$4
 
-curl -X POST --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ }'
+curl -X POST --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ "inputs": { "should_fail": false } }'
