fd8e0a4eeb976554d8f30701fd842fb430c0b8b4
svetaquali <svetlana.c@quali.com>
2025-06-29 13:22:05 +0300

    check success

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -3,4 +3,9 @@ torque_token=$2
 space=$3
 env_id=$4
 
-curl -X POST --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ "inputs": { "should_fail": false } }'
+http_code=$(curl -X POST -o response.txt -w %{http_code} --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ "inputs": { "should_fail": false } }')
+cat response.txt
+if [ $http_code != "200" ]; then
+ echo "update got bad status code $http_code, exiting with error"
+ exit 1;
+fi
