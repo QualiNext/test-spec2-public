667ec512166f197177a76fa0496d665f68e89526
svetaquali <svetlana.c@quali.com>
2025-06-29 10:45:46 +0300

    use update_env script

---
+++ scripts/update_env.sh
@@ -0,0 +1,6 @@
+torque_server_address=$1
+torque_token=$2
+space=$3
+env_id=$4
+
+curl -X POST --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ }'
