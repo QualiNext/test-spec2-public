fb5fa5b6cfac56b5ad5206c28a157b618871faaa
svetaquali <svetlana.c@quali.com>
2025-06-29 13:38:37 +0300

    force update

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -3,7 +3,7 @@ torque_token=$2
 space=$3
 env_id=$4
 
-http_code=$(curl -X POST -o response.txt -w %{http_code} --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ "inputs": { "should_fail": false } }')
+http_code=$(curl -X POST -o response.txt -w %{http_code} --url "$torque_server_address/api/spaces/$space/environments/$env_id/update_v2" -H "Authorization: Bearer $torque_token" -H 'Content-Type: application/json' --data '{ "inputs": { "should_fail": false }, "force": true }')
 cat response.txt
 if [ $http_code != "200" ]; then
  echo "update got bad status code $http_code, exiting with error"
