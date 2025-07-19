98f6b9b70733a009ad56e51e863915617dc77cf4
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2025-07-19 19:14:24 +0300

    added source 'GQ7O6JM4_MAJ08X'

---
+++ torque-generated-sources/GQ7O6JM4_MAJ08X/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_network" "res-0" {
+  address_space = [
+    "10.0.0.0/16"
+  ]
+  location = "eastus"
+  name = "demo-vnet"
+  resource_group_name = "demo-group"
+  tags = {
+    Environmnet = "infra"
+  }
+}
